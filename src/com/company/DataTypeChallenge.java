feccd63e42287c76471d1df10097b555e702b24a
kateosborn <katelosborn@gmail.com>
2020-06-15 21:52:30 -0500

    this is update 06/15/2020

---
+++ src/com/company/DataTypeChallenge.java
@@ -0,0 +1,16 @@
+package com.company;
+/*
+pseudo code:
+-import scanner
+-create 3 variables
+    - int, double, String
+-use your scanner to initialize the 3 varibles (w the correct method)
+ -Print the sum of int plus your int variable on a new line.
+ - Print the sum of double plus your double variable to a scale of one decimal place on a new line.
+- Concatenate string with the string you read as input and print the result on a new line.
+ */
+public class DataTypeChallenge {
+    public static void main(String[] args) {
+
+    }
+}
