b6b37d664ee2c8fe72991af649af5cfdcec4519f
kateosborn <katelosborn@gmail.com>
2020-06-16 21:43:23 -0500

    this is update 06/16/2020

--- src/com/company/DataTypeChallenge.java
+++ src/com/company/DataTypeChallenge.java
@@ -8,13 +8,15 @@ Kate Osborn
 
 PseudoCode:
 -import scanner
-- create the scanner
--create 3 variables
+-create the scanner
+-establish the varibles given (int s, double d, String s)
+-create 3 new variables:
     - int, double, String
--use your scanner to initialize the 3 varibles (w the correct method)
+-Write the code so you can use your scanner to type out what you want (new integer, new double, new string)
  -Print the sum of int plus your int variable on a new line.
  - Print the sum of double plus your double variable to a scale of one decimal place on a new line.
 - Concatenate string with the string you read as input and print the result on a new line.
+- Write the line of code to add them together so they print!
  */
 public class DataTypeChallenge {
     public static void main(String[] args) {
